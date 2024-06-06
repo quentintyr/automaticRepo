a19692cd2bf996db04963803af1bed05e714991a
quentintyr <quentin.tyr@gmail.com>
2024-06-06 22:42:01 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("This is just a placeholder for the repository.");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
